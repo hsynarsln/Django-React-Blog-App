b2dc303d4a6f15b496b3dd804e39eb96a4a28dcd
hsynarslan <hsyn06arslan@gmail.com>
2022-03-19 15:47:18 +0300

    card hover styles

--- client/src/components/BlogCard.js
+++ client/src/components/BlogCard.js
@@ -10,36 +10,38 @@ const BlogCard = ({ data }) => {
   const navigate = useNavigate();
 
   return (
-    <Card sx={{ maxWidth: 375 }} onClick={() => navigate(`/detail/${data.id}`)}>
-      <CardHeader
-        avatar={
-          <Avatar sx={{ bgcolor: grey[900] }} aria-label='recipe'>
-            {data.author[0].toUpperCase() || 'A'}
-          </Avatar>
-        }
-        title={data.title.length > 30 ? data.title.substring(0, 30) + '...' : data.title}
-        subheader={
-          <div style={{ display: 'flex', flexDirection: 'row', justifyContent: 'space-between' }}>
-            <Typography variant='body2' color='text.secondary'>
-              {data.category}
-            </Typography>
-            <Typography variant='body2' color='text.secondary'>
-              {data.days_since_creation > 0 ? data.days_since_creation + ' days ago' : 'today'}
-            </Typography>
-          </div>
-        }
-      />
-      <CardMedia component='img' height='194' image={data.image} alt={data.title} />
-      <CardContent>
-        <Typography variant='body2' color='text.secondary' className='line-clamp'>
-          {data.content}
-        </Typography>
-      </CardContent>
-      <CardContent>
-        <Typography variant='body1' color='text.secondary' style={{ textAlign: 'right', fontWeight: 'bold' }}>
-          {data.author || 'Anonymous'}
-        </Typography>
-      </CardContent>
+    <Card sx={{ maxWidth: 375 }} className='card'>
+      <div onClick={() => navigate(`/detail/${data.id}`)}>
+        <CardHeader
+          avatar={
+            <Avatar sx={{ bgcolor: grey[900] }} aria-label='recipe'>
+              {data.author[0].toUpperCase() || 'A'}
+            </Avatar>
+          }
+          title={data.title.length > 30 ? data.title.substring(0, 30) + '...' : data.title}
+          subheader={
+            <div style={{ display: 'flex', flexDirection: 'row', justifyContent: 'space-between' }}>
+              <Typography variant='body2' color='text.secondary'>
+                {data.category}
+              </Typography>
+              <Typography variant='body2' color='text.secondary'>
+                {data.days_since_creation > 0 ? data.days_since_creation + ' days ago' : 'today'}
+              </Typography>
+            </div>
+          }
+        />
+        <CardMedia component='img' height='194' image={data.image} alt={data.title} />
+        <CardContent>
+          <Typography variant='body2' color='text.secondary' className='line-clamp'>
+            {data.content}
+          </Typography>
+        </CardContent>
+        <CardContent>
+          <Typography variant='body1' color='text.secondary' style={{ textAlign: 'right', fontWeight: 'bold' }}>
+            {data.author || 'Anonymous'}
+          </Typography>
+        </CardContent>
+      </div>
 
       <CardActions disableSpacing>
         <IconButton aria-label='add to favorites'>
