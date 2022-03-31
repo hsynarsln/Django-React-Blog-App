813af0da23602fefc4a691a12197af24d9268e99
hsynarslan <hsyn06arslan@gmail.com>
2022-03-31 20:56:25 +0300

    react v18 suspense

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -1,5 +1,5 @@
-import { Button, CircularProgress, Container, Grid, Grow } from '@mui/material';
-import React, { useEffect } from 'react';
+import { Button, Container, Grid, Grow } from '@mui/material';
+import React, { Suspense, useEffect } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import BlogCard from '../components/BlogCard';
 import Loader from '../components/Loader';
@@ -33,9 +33,7 @@ const Home = () => {
       <Container>
         <Grid container justify='space-between' alignItems='stretch' spacing={3}>
           <Grid item xs={12} sm={12} md={12}>
-            {loading ? (
-              <Loader />
-            ) : (
+            <Suspense fallback={<Loader />}>
               <Grid container rowSpacing={3} columnSpacing={{ xs: 1, sm: 2, md: 3 }} style={{ marginTop: '5rem' }} spacing={2}>
                 {blogs?.map(data => (
                   <Grid key={data.id} item xs={12} md={4} sm={6} justifyContent='center'>
@@ -43,15 +41,13 @@ const Home = () => {
                   </Grid>
                 ))}
               </Grid>
-            )}
+            </Suspense>
           </Grid>
-          {moreLoading ? (
-            <CircularProgress style={{ display: 'flex', margin: '0 auto 2rem auto' }} color='inherit' />
-          ) : (
+          <Suspense fallback={<h1>Loading...</h1>}>
             <Button disabled={next === null} onClick={loadMoreData} style={{ display: 'flex', margin: '2rem auto 2rem auto' }} variant='contained' color='secondary'>
               Load More Post
             </Button>
-          )}
+          </Suspense>
         </Grid>
       </Container>
     </Grow>
